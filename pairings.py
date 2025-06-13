8865a638d3535e5bf81b791c231e91a20ed233bf
FloritaZhao <feifeilan@hotmail.com>
2025-06-13 10:50:52 -0700

    problems completed

--- pairings.py
+++ pairings.py
@@ -1,6 +1,36 @@
 def find_pairs(num_string):
+
+  # 1: convert the string into a list of integer
+  new_string = num_string.split(" ")
+  num_list = []
+  for string in new_string:
+    num_list.append(int(string))
+  
+
+  if len(num_list) < 2:
+    return set()
+  # 2: initiate a set to store the results
+  paring_set = set()
+
+  # 3, for i in the list,
+  for i in range(len(num_list)-1):
+    for j in range(i+1, len(num_list)):
+      if num_list[i] < num_list[j]:
+        new_pair = (num_list[i], num_list[j])
+        paring_set.add(new_pair)
+      else: 
+        new_pair = (num_list[j], num_list[i])
+        paring_set.add(new_pair)
+
+  return paring_set
+  
+      
+  # 4, for j in the range (i, len(list))
+  # 5, check if (i, j) is the result, i, j is in the right order, i < j
+  # 6, return the set
+
   # Write your solution here!
-  pass
+ 
 
 
 # Test cases
